0201af60cb9014457a64980e7e1ffd9c6c6f07b9
Lovesee, Pierce <pierce.lovesee@dish.com>
2022-08-02 15:47:50 -0600

    removed pycache folders and corrected test cases

--- msspackages/bucketization/__init__.py
+++
@@ -1,1 +0,0 @@
-from .bucketization import bucko
--- tests/test_parity.py
+++ tests/test_parity.py
@@ -1,6 +1,10 @@
-from msspackages import *
+import msspackages as mss
 
-number1 = Number(5)
+num_odd = mss.Number(5)
+num_even = mss.Number(2)
 
-def test_getters():
-    assert number1.parity == 'odd
+def test_odd():
+    assert num_odd.parity() == 'odd'
+
+def test_even():
+    assert num_even.parity() == 'even'
