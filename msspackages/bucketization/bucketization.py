b0b17cd6837a83ef57d19b3a8a82fd55df837280
vinayak.sharma <vinayak.sharma@dish.com>
2022-08-02 11:19:50 -0600

    add the REAL bucketization

--- msspackages/bucketization/bucketization.py
+++ msspackages/bucketization/bucketization.py
@@ -4,7 +4,38 @@ from datetime import datetime
 from datetime import timedelta
 from tqdm import tqdm
 
+##grouped = df.groupby('instance_id').resample('H').agg({'node_filesystem_utilization':['max', 'var']})
 
-def bucko(x):
+## metrics needs to be a list of all metrics that you want to bucketize in the dataframe. NO categorical features allowed
 
-   print((x))
+##aggregated_outputs needs to be a list of lists, where each output needs to be a list. 
+    ## ex/ [['max'],['min'],['max',var]]
+
+##this bucketization algorithm operates on a node level. Can be modified to operate on a pod/container/namespace level. 
+
+##aggregated_outputs need to be the same size as metrics
+
+##metric_epochtime is a requirement
+
+##groupby needs to be a string
+
+##use the pd.resample bucket string formatting for bucketsize 
+
+
+
+def bucketization(df,groupby, metrics, aggregated_outputs, bucket_size):
+    
+    df = df.set_index('metric_epochtime')
+    
+    
+    aggregate_dictionary = {}
+    ##create a dictionary to agregate by 
+    for i in range(len(aggregated_outputs)):
+        aggregate_dictionary[metrics[i]] = aggregated_outputs[i]
+        
+    grouped = df.groupby(groupby).resample(bucket_size).agg(aggregate_dictionary)
+    
+    return grouped
+    
+    
+    
