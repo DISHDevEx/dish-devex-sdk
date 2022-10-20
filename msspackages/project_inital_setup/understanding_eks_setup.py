1875213b13fe669cabac51868533f33eba1f478c
praveen mada <praveen.mada@dish.com>
2022-10-20 22:08:15 +0000

    cleanup

--- msspackages/project_inital_setup/understanding_eks_setup.py
+++ msspackages/project_inital_setup/understanding_eks_setup.py
@@ -1,4 +1,5 @@
-import os     
+import os
+import sys
 import subprocess
 from subprocess import Popen, PIPE
 
@@ -10,10 +11,8 @@ def setup_runner(setup_type = 'notebook' , project = 'understanding-eks-data'):
             process = subprocess.Popen([os.path.join(os.path.dirname(__file__), "notebook_setup_run.sh")], stdout=PIPE, stderr=PIPE, shell=True)
         elif setup_type == 'console':
              process = subprocess.Popen([os.path.join(os.path.dirname(__file__), "console_setup_run.sh")], stdout=PIPE, stderr=PIPE, shell=True)
-
+    
     stdout, stderr = process.communicate()
     if stdout:
-        print(stdout)
-    # if stderr:
-    #     raise Exception("Error "+str(stderr))
+        print(str(stdout) + '\n')
         
