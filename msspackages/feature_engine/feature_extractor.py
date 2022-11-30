be80debc7ffb2f9542fb8a42c30af58a659d7bf6
praveen mada <praveen.mada@dish.com>
2022-11-30 23:49:42 +0000

    updates to get features

--- msspackages/feature_engine/feature_extractor.py
+++ msspackages/feature_engine/feature_extractor.py
@@ -7,11 +7,11 @@ from glob import glob
 
 
 def get_features(input_feature_group_name = "", input_created_date = ""):
+
     ##feature schemas
     ## Read the feature schema for the feature store
     try:
         all_features_path = glob(os.path.join(os.path.dirname(__file__), "eks_feature_store", "*.json"))
-        #print(all_features_path)
 
         for count,file_name in enumerate(all_features_path):
             with open(file_name) as f:
@@ -23,8 +23,6 @@ def get_features(input_feature_group_name = "", input_created_date = ""):
                     features_df =  features_df.append(pd.json_normalize(data=feature_data, record_path='features_list', 
                                 meta=['feature_group_name', 'feature_group_description', 'model_type', 'problem_type',  'created_by', 'created_date', 'model_parameters']))
 
-        # pd.set_option('display.max_columns', None)
-        # print(features_df)
 
         if input_feature_group_name != "" and input_created_date != "":
             features_df = features_df[(features_df['feature_group_name'] == input_feature_group_name) & (features_df['created_date'] == input_created_date)]
