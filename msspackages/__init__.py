0e966d37f28d46bfce4913005cc3fd2fb7391e6f
David Cherney <david.cherney@dish.com>
2023-01-11 02:18:48 +0000

    The module (file) CircleClass.py did not follow pep naming conventions. It has been renamed to circles.py

--- msspackages/__init__.py
+++ msspackages/__init__.py
@@ -2,7 +2,7 @@ from .add import add
 from .subtract import subtract
 from .extras import multiply, divide
 
-from .circles import CirclesClass
+from .circles import circles
 from .circles import Circle
 
 from .parity import NumberClass
--- msspackages/circles/__init__.py
+++ msspackages/circles/__init__.py
@@ -1,2 +1,8 @@
-from .CirclesClass import Circle
-from .CirclesClass import describe
+"""
+A class and a function acting on objects from that class
+for the purposes of demonstration of contributing to MSSPackages
+"""
+
+
+from .circles import Circle
+from .circles import describe
