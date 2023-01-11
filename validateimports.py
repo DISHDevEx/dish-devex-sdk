657d4e282d5d5158980665e5b44933322ad0c9e6
David Cherney <david.cherney@dish.com>
2023-01-11 16:51:26 +0000

    fixed create_spark_utils issues wherein pkg_list had [] as default; changed default to None.

--- validateimports.py
+++ validateimports.py
@@ -7,5 +7,4 @@ from msspackages import Circle
 from msspackages import Pyspark_data_ingestion
 from msspackages import setup_runner
 from msspackages import get_features
-# needs debugging:
 from msspackages import describe
