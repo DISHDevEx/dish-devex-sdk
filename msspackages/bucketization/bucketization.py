a7e5aa5d02f2b6e4a3308b4d8457e9b0f2956827
David Cherney <david.cherney@dish.com>
2023-01-11 01:30:47 +0000

    linting of validateimports.py

--- msspackages/bucketization/bucketization.py
+++ msspackages/bucketization/bucketization.py
@@ -8,18 +8,21 @@ from tqdm import tqdm
 def eks_bucketization(df,groupby, metrics, aggregated_outputs, bucket_size):
     """
         Inputs:
-            metrics needs to be a list of all metrics that you want to bucketize in the dataframe. NO categorical features allowed
+            metrics needs to be a list of all metrics that you want to bucketize 
+            in the dataframe. NO categorical features allowed
 
-            aggregated_outputs needs to be a list of lists, where each output needs to be a list. 
+            aggregated_outputs needs to be a list of lists, where each output 
+            needs to be a list. 
                 ex/ [['max'],['min'],['max',var]]
 
-            this bucketization algorithm operates on a node level. Can be modified to operate on a pod/container/namespace level. 
+            this bucketization algorithm operates on a node level but can be 
+            modified to operate on a pod/container/namespace level. 
 
-            aggregated_outputs need to be the same size as metrics
+            aggregated_outputs need to be the same size as metrics.
 
-            metric_epochtime is a requirement
+            metric_epochtime is requiremed.
 
-            groupby needs to be a string
+            groupby needs to be a string.
 
             use the pd.resample bucket string formatting for bucketsize 
 
--- validateimports.py
+++ validateimports.py
@@ -1,27 +1,11 @@
-# from msspackages import bucketization
-# bucketization.bucko("good job")
+"""
+Test that imports do not throw errors.
+"""
 
-
-
-
-# from msspackages.bucketization import bucko
-# bucko("bad job")
-
+from msspackages import Number
 from msspackages import Circle
 from msspackages import Pyspark_data_ingestion
 from msspackages import setup_runner
 from msspackages import get_features
-
-# ci = Circle(5,"red")
-
-# #ci.describe(ci)
-
-# setup_runner('notebook')
-
-# df_build = Pyspark_data_ingestion(year = '2022', month = '5', day = '5', hour = '5', filter_column_value = 'Cluster')
-# spark = df_build.get_spark()
-# df_err, df =  df_build.read()
-# if df_err == 'PASS':
-#     df.show()
-
-print(get_features())
+# needs debugging:
+# from msspackages import describe
