57f2a41bd4d4ce3ee40131ce77a9a26c5e008fca
David Cherney <david.cherney@dish.com>
2023-01-11 02:52:17 +0000

    linting of update_cwd_to_root

--- msspackages/update_cwd/notebook_setup_path.py
+++ msspackages/update_cwd/notebook_setup_path.py
@@ -1,32 +1,39 @@
+"""
+This function, intended to be run in jupyter notebooks/labs,
+changes the path of the current working directory to
+the specified input parameter.
+"""
+
+
 import os
 
+
 def update_cwd_to_root(root_repository = 'understanding-eks-data'):
-    
+
     """
-    This function changes the path of the current working directory to the specified input parameter.
-    
+    Change the current working directory of jupyter notebook/lab.
+
     Parameters
     ----------
     root_repository : str
         Root repository name
-    
+
     Returns
     -------
     err_msg : str or None
         Path does not exist
 
     """
-    
+
     cwd = os.getcwd()
     print('current repository:', cwd)
-    
+
     if root_repository not in cwd.split('/'):
         #in case root_repository does not exist
-        raise IOError(f'Path does not exist, please check root_repository name entered: {root_repository}')
-        
-    else:
-        
-        while not(cwd.endswith(root_repository)):
-            os.chdir("..")
-            cwd = os.getcwd()
-            print('current repository:', cwd)
+        raise IOError('The requested directory, '
+                      + f'{root_repository} does not exist.')
+
+    while not cwd.endswith(root_repository):
+        os.chdir("..")
+        cwd = os.getcwd()
+        print('The new current working directory is', cwd)
