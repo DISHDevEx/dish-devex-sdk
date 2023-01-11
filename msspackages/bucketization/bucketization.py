36766d8b70746024768a9751bbb91100ea42f4c7
David Cherney <david.cherney@dish.com>
2023-01-11 02:07:11 +0000

    linted bucketization.py

--- msspackages/bucketization/bucketization.py
+++ msspackages/bucketization/bucketization.py
@@ -1,22 +1,24 @@
+"""
+Resample timeseries to desired period; each time interval of period width
+is a bucket. Hence bucketization.
+"""
+
+
 import pandas as pd
-import numpy as np
-from datetime import datetime
-from datetime import timedelta
-from tqdm import tqdm
 
 
-def eks_bucketization(df,groupby, metrics, aggregated_outputs, bucket_size):
+def eks_bucketization(input_df, groupby, metrics, aggregated_outputs, bucket_size):
     """
         Inputs:
-            metrics needs to be a list of all metrics that you want to bucketize 
-            in the dataframe. NO categorical features allowed
+            metrics needs to be a list of all metrics that you want to
+            bucketize in the dataframe. NO categorical features allowed
 
-            aggregated_outputs needs to be a list of lists, where each output 
-            needs to be a list. 
+            aggregated_outputs needs to be a list of lists, where each output
+            needs to be a list.
                 ex/ [['max'],['min'],['max',var]]
 
-            this bucketization algorithm operates on a node level but can be 
-            modified to operate on a pod/container/namespace level. 
+            this bucketization algorithm operates on a node level but can be
+            modified to operate on a pod/container/namespace level.
 
             aggregated_outputs need to be the same size as metrics.
 
@@ -24,24 +26,22 @@ def eks_bucketization(df,groupby, metrics, aggregated_outputs, bucket_size):
 
             groupby needs to be a string.
 
-            use the pd.resample bucket string formatting for bucketsize 
+            use the pd.resample bucket string formatting for bucketsize
 
         Outputs:
-        
-            Pandas DataFrame - df.groupby(groupby).resample(bucket_size).agg(aggregate_dictionary)
+
+            Pandas DataFrame - input_df.groupby(groupby).resample(bucket_size)\
+                                 .agg(aggregate_dictionary)
     """
-    
-    df = df.set_index('metric_epochtime')
-    
-    
+
+    assert isinstance(input_df, pd.DataFrame)
+    input_df = input_df.set_index('metric_epochtime')
+    ##create a dictionary to agregate by
     aggregate_dictionary = {}
-    ##create a dictionary to agregate by 
-    for i in range(len(aggregated_outputs)):
-        aggregate_dictionary[metrics[i]] = aggregated_outputs[i]
-        
-    grouped = df.groupby(groupby).resample(bucket_size).agg(aggregate_dictionary)
-    
+    for i,aggregated_output in enumerate(aggregated_outputs):
+        aggregate_dictionary[metrics[i]] = aggregated_output
+
+    grouped = input_df.groupby(groupby).resample(bucket_size)\
+        .agg(aggregate_dictionary)
+
     return grouped
-    
-    
-    
