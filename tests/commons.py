6b7d8a93873d168bf4061519664411f31240afa3
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-02-01 15:06:33 -0700

    added sum for counts test

--- tests/commons.py
+++ tests/commons.py
@@ -6,8 +6,8 @@ from pyspark.sql.functions import to_json
 # for pyspark job
 #bucket_name = "hamza-test-public"
 bucket_name = "hamza-sagemaker"
-#folder_name = "test_data/part-00000-c83945eb-9667-46a6-855e-547c88e5c61c-c000.snappy.parquet"
-folder_name = "test_data/"
+folder_name = "test_data/part-00000-c83945eb-9667-46a6-855e-547c88e5c61c-c000.snappy.parquet"
+#folder_name = "test_data/"
 
 #for Dask job
 s3_link_dask = "s3a://hamza-sagemaker/test_data/part-00000-c83945eb-9667-46a6-855e-547c88e5c61c-c000.snappy.parquet"
@@ -24,7 +24,7 @@ def merge_master_schema(name, Schema, Spark, Spark_context):
                                  schema = Schema)
 
     merged_df = data_fail.unionByName(master_schema_json, allowMissingColumns=True)
-    obj = EKS_Connector(bucket_name,folder_name)
+    obj = EKS_Connector(bucket_name,folder_name, setup="github_actions")
     for item in obj.find_multilevel_schema_items(schema=merged_df.schema):
         merged_df = merged_df.withColumn(item, to_json(merged_df[item]))
         
