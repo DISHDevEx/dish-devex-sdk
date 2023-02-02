67e62f34ac67a69ce33e76d0cb57afab7b9642ed
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-02-02 11:20:43 -0700

    updated conf setting for aws

--- tests/commons.py
+++ tests/commons.py
@@ -1,7 +1,7 @@
 import os
 from devex_sdk import EKS_Connector
 from pyspark.sql.functions import to_json
-
+from pyspark.sql.types import *
 
 # for pyspark job
 #bucket_name = "hamza-test-public"
@@ -14,14 +14,26 @@ s3_link_dask = "s3a://hamza-sagemaker/test_data/part-00000-c83945eb-9667-46a6-85
 
 
 # resuable function to merge master schema to filelds outside of the log messages
-def merge_master_schema(name, Schema, Spark, Spark_context):
+def merge_master_schema(name, Spark, Spark_context):
     
     #master_schema_path = f"/home/runner/work/dish-devex-sdk/dish-devex-sdk/devex_sdk/data_ingestion/container_insights_schema/{name}.json"
     master_schema_path = f"devex_sdk/data_ingestion/container_insights_schema/{name}.json"
     master_schema_json = Spark.read.json(master_schema_path, multiLine=True)
     print(master_schema_path)
     print(master_schema_json)
-    data_fail = Spark.createDataFrame(data=[], schema=Schema)
+    eks_performance_logs_schema_test = StructType(
+        [StructField("account_id", StringType(), True),  # col1
+         StructField("log_group_name", StringType(), True),  # col2 .. etc
+         StructField("log_stream_name", StringType(), True),
+         StructField("record_id", StringType(), True),
+         StructField("stream_name", StringType(), True),
+         StructField("record_arrival_stream_timestamp", TimestampType(), True),
+         StructField("record_arrival_stream_epochtime", LongType(), True),
+         StructField("log_event_timestamp", TimestampType(), True),
+         StructField("log_event_epochtime", LongType(), True),
+         StructField("log_event_id", StringType(), True),
+         StructField("region", StringType(), True), ])
+    data_fail = Spark.createDataFrame(data=[], schema=eks_performance_logs_schema_test)
 
 
 
