f50fc07850840497d49f95fc50ca8fb339e2a215
Tomer Danon <tomer.danon@dish.com>
2023-04-10 17:12:11 +0000

    updated code

--- devex_sdk/data_ingestion/gz_connector/conftest.py
+++ devex_sdk/data_ingestion/gz_connector/conftest.py
@@ -18,7 +18,7 @@ def s3_resource():
 def read_s3(filename):
     s3_resource = boto3.resource('s3')
     bucket = 'respons-logs'
-    key = f'pytest/{filename}.txt'
+    key = f'pytest/txt_files/{filename}_contents.txt'
     obj = s3_resource.Object(bucket_name=bucket, key=key)
     body = obj.get()['Body'].read().decode('UTF-8')
     
@@ -27,7 +27,7 @@ def read_s3(filename):
 def read_df_s3(filename):
     s3_client = boto3.client('s3')
     bucket = 'respons-logs'
-    key = f'pytest/{filename}_df.pickle'
+    key = f'pytest/pickle_files/{filename}_df.pickle'
     response = s3_client.get_object(Bucket=bucket, Key=key)
     body = response['Body'].read()
     df = pickle.loads(body)
@@ -40,36 +40,36 @@ def read_df_s3(filename):
 
 @pytest.fixture(scope='module')
 def get_paths_expected():
-    return ['respons-logs/pytest/application.gz', 'respons-logs/pytest/authenticator.gz',
-            'respons-logs/pytest/cloud_controller_manager.gz','respons-logs/pytest/cluster.gz',
-            'respons-logs/pytest/clusternamespace.gz', 'respons-logs/pytest/clusterservice.gz',
-            'respons-logs/pytest/container.gz', 'respons-logs/pytest/containerfs.gz',
-            'respons-logs/pytest/host.gz', 'respons-logs/pytest/kube_apiserver.gz',
-            'respons-logs/pytest/kube_controller_manager.gz', 'respons-logs/pytest/kube_scheduler.gz',
-            'respons-logs/pytest/node.gz', 'respons-logs/pytest/nodediskio.gz',
-            'respons-logs/pytest/nodefs.gz', 'respons-logs/pytest/nodenet.gz',
-            'respons-logs/pytest/pod.gz', 'respons-logs/pytest/podnet.gz']
+    return ['respons-logs/pytest/application.gz', 'respons-logs/pytest/gz_files/authenticator.gz',
+                'respons-logs/pytest/cloud_controller_manager.gz','respons-logs/pytest/gz_files/cluster.gz',
+                'respons-logs/pytest/clusternamespace.gz', 'respons-logs/pytest/gz_files/clusterservice.gz',
+                'respons-logs/pytest/container.gz', 'respons-logs/pytest/gz_files/containerfs.gz',
+                'respons-logs/pytest/host.gz', 'respons-logs/pytest/gz_files/kube_apiserver.gz',
+                'respons-logs/pytest/kube_controller_manager.gz', 'respons-logs/pytest/gz_files/kube_scheduler.gz',
+                'respons-logs/pytest/node.gz', 'respons-logs/pytest/gz_files/nodediskio.gz',
+                'respons-logs/pytest/nodefs.gz', 'respons-logs/pytest/gz_files/nodenet.gz',
+                'respons-logs/pytest/pod.gz', 'respons-logs/pytest/gz_files/podnet.gz']
 
 @pytest.fixture(scope='module')
 def get_objects_expected(s3_resource):
-    return [s3_resource.Object(bucket_name='respons-logs', key='pytest/application.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/authenticator.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/cloud_controller_manager.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/cluster.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/clusternamespace.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/clusterservice.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/container.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/containerfs.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/host.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/kube_apiserver.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/kube_controller_manager.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/kube_scheduler.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/node.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/nodediskio.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/nodefs.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/nodenet.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/pod.gz'),
-            s3_resource.Object(bucket_name='respons-logs', key='pytest/podnet.gz')]
+    return [s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/application.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/authenticator.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/cloud_controller_manager.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/cluster.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/clusternamespace.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/clusterservice.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/container.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/containerfs.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/host.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/kube_apiserver.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/kube_controller_manager.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/kube_scheduler.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/node.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/nodediskio.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/nodefs.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/nodenet.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/pod.gz'),
+            s3_resource.Object(bucket_name='respons-logs', key='pytest/gz_files/podnet.gz')]
 
 @pytest.fixture(scope='module')
 def process_objects_expected():
@@ -77,7 +77,7 @@ def process_objects_expected():
 
 @pytest.fixture(scope='module')
 def performance_contents():
-    return read_s3('performance_contents')
+    return read_s3('performance')
 
 @pytest.fixture(scope='module')
 def init_performance_expected_df():
@@ -86,7 +86,7 @@ def init_performance_expected_df():
 @pytest.fixture(scope='module')
 def application_contents():
     # return [['2023-03-23T15:09:32.282Z {"log":"2023-03-23T15:09:32.28284833Z stderr F 2023-03-23T15:09:32Z I! no pod is found for namespace:default,podName:open5gs-udr-6c96f5d447-8gfmb, refresh the cache now...","kubernetes":{"pod_name":"cloudwatch-agent-spj2z","namespace_name":"default","pod_id":"c7d7e86f-68d2-420f-b563-91161f2e0e4f","host":"ip-10-0-101-15.ec2.internal","container_name":"cloudwatch-agent","docker_id":"d4bba41a8063a3702a918a7015395fd3fd6adfe1488662ea1996115da4cfda44","container_hash":"docker.io/amazon/cloudwatch-agent@sha256:33f0072c93d614b5dd32f044549f3d764d05a42f068e852e94bdd849098852c7","container_image":"docker.io/amazon/cloudwatch-agent:1.247354.0b251981"}}']]
-    return read_s3('application_contents')
+    return read_s3('application')
 
 @pytest.fixture(scope='module')
 def init_application_expected_df():
@@ -99,7 +99,7 @@ def init_application_expected_df():
 @pytest.fixture(scope='module')
 def kube_scheduler_contents():
     # return [['2023-03-23T15:07:05.000Z I0323 15:07:05.946516      12 schedule_one.go:266] "Successfully bound pod to node" pod="openverso/ueransim-gnb-84c9b57c64-tz46g" node="ip-10-0-102-34.ec2.internal" evaluatedNodes=6 feasibleNodes=6']]
-    return read_s3('cp_scheduler_contents')
+    return read_s3('cp_scheduler')
 
 @pytest.fixture(scope='module')
 def kube_scheduler_expected_df():
@@ -116,7 +116,7 @@ def kube_scheduler_expected_df():
 @pytest.fixture(scope='module')
 def kube_controller_manager_contents():
     # return [['2023-03-23T15:00:23.000Z E0323 15:00:23.228069      11 horizontal.go:226] failed to compute desired number of replicas based on listed metrics for Deployment/openverso/open5gs-amf: invalid metrics (1 invalid out of 1), first error is: failed to get cpu resource metric value: failed to get cpu utilization: unable to get metrics for resource cpu: unable to fetch metrics from resource metrics API: the server could not find the requested resource (get pods.metrics.k8s.io)']]
-    return read_s3('cp_scheduler_contents')
+    return read_s3('cp_kube_controller')
 
 @pytest.fixture(scope='module')
 def init_cp_kube_controller_expected_df():
@@ -134,6 +134,7 @@ def init_cp_kube_controller_expected_df():
 @pytest.fixture(scope='module')
 def kube_apiserver_contents():
     return [['2023-03-23T15:09:30.000Z I0323 15:09:30.969664      11 alloc.go:327] "allocated clusterIPs" service="default/open5gs-amf-ngap" clusterIPs=map[IPv4:172.20.188.198]']]
+    return read_s3('cp_api')
 
 @pytest.fixture(scope='module')
 def init_cp_api_expected_df():
@@ -141,11 +142,12 @@ def init_cp_api_expected_df():
     #     ['2023-03-23T15:09:30.000Z I0323 15:09:30.969664', '11 alloc.go:327] "allocated clusterIPs" service="default/open5gs-amf-ngap" clusterIPs=map[IPv4:172.20.188.198]']]
     # df = pd.DataFrame(data, columns=['log_timestamp', 'data'])
     # return df
-    return read_df_s3('cp_kube_controller')
+    return read_df_s3('cp_api')
 
 @pytest.fixture(scope='module')
 def authenticator_contents():
-    return [['2023-03-23T15:00:41.719Z time="2023-03-23T15:00:41Z" level=info msg="STS response" accesskeyid=ASIAQ52MI263MY5EWZW2 accountid=064047601590 arn="arn:aws:sts::064047601590:assumed-role/test_node_group_role/i-0429e7bc1c0bb2932" client="127.0.0.1:52552" method=POST path=/authenticate session=i-0429e7bc1c0bb2932 userid=AROAQ52MI263K3VH2HACL']]
+    # return [['2023-03-23T15:00:41.719Z time="2023-03-23T15:00:41Z" level=info msg="STS response" accesskeyid=ASIAQ52MI263MY5EWZW2 accountid=064047601590 arn="arn:aws:sts::064047601590:assumed-role/test_node_group_role/i-0429e7bc1c0bb2932" client="127.0.0.1:52552" method=POST path=/authenticate session=i-0429e7bc1c0bb2932 userid=AROAQ52MI263K3VH2HACL']]
+    return read_s3('cp_authenticator')
 
 @pytest.fixture(scope='module')
 def init_cp_authenticator_expected_df():
@@ -156,7 +158,8 @@ def init_cp_authenticator_expected_df():
 
 @pytest.fixture(scope='module')
 def cloud_controller_manager_contents():
-    return [['2023-03-23T15:02:49.000Z I0323 15:02:49.728729      11 tagging_controller.go:152] Skip putting node ip-10-0-101-15.ec2.internal in work queue since it was already tagged earlier.']]
+    # return [['2023-03-23T15:02:49.000Z I0323 15:02:49.728729      11 tagging_controller.go:152] Skip putting node ip-10-0-101-15.ec2.internal in work queue since it was already tagged earlier.']]
+    return read_s3('cp_cloud_controller')
 
 @pytest.fixture(scope='module')
 def init_cp_cloud_controller_expected_df():
@@ -167,7 +170,8 @@ def init_cp_cloud_controller_expected_df():
 
 @pytest.fixture(scope='module')
 def dataplane_contents():
-    return [['''2023-03-23T15:05:36.671Z {"hostname":"ip-10-0-101-15.ec2.internal","systemd_unit":"kubelet.service","message":"E0323 15:05:36.670951    6922 remote_runtime.go:734] \"ExecSync cmd from runtime service failed\" err=\"rpc error: code = DeadlineExceeded desc = failed to exec in container: timeout 5s exceeded: context deadline exceeded\" containerID=\"ac5e4fff7402033c291ba30ab91bff331c12bdea79c8dadedf73e5cf14af71a7\" cmd=[/bitnami/scripts/readiness-probe.sh]","az":"us-east-1a","ec2_instance_id":"i-03672fb3a91ec2337"}''']]
+    # return [['''2023-03-23T15:05:36.671Z {"hostname":"ip-10-0-101-15.ec2.internal","systemd_unit":"kubelet.service","message":"E0323 15:05:36.670951    6922 remote_runtime.go:734] \"ExecSync cmd from runtime service failed\" err=\"rpc error: code = DeadlineExceeded desc = failed to exec in container: timeout 5s exceeded: context deadline exceeded\" containerID=\"ac5e4fff7402033c291ba30ab91bff331c12bdea79c8dadedf73e5cf14af71a7\" cmd=[/bitnami/scripts/readiness-probe.sh]","az":"us-east-1a","ec2_instance_id":"i-03672fb3a91ec2337"}''']]
+    return read_s3('dataplane')
 
 @pytest.fixture(scope='module')
 def init_dataplane_expected_df():
@@ -175,11 +179,12 @@ def init_dataplane_expected_df():
     # df = pd.DataFrame(data, columns=['log_timestamp', 'data'])
     # # df['data'] = df.data.apply(json.loads)
     # return df
-    return read_df_s3('cp_kube_controller')
+    return read_df_s3('dataplane')
 
 @pytest.fixture(scope='module')
 def host_contents():
-    return [['2023-03-23T15:05:36.000Z {"host":"ip-10-0-101-15","ident":"kubelet","message":"E0323 15:05:36.670951    6922 remote_runtime.go:734] \\"ExecSync cmd from runtime service failed\\" err=\\"rpc error: code = DeadlineExceeded desc = failed to exec in container: timeout 5s exceeded: context deadline exceeded\\" containerID=\\"ac5e4fff7402033c291ba30ab91bff331c12bdea79c8dadedf73e5cf14af71a7\\" cmd=[/bitnami/scripts/readiness-probe.sh]","az":"us-east-1a","ec2_instance_id":"i-03672fb3a91ec2337"}']]
+    # return [['2023-03-23T15:05:36.000Z {"host":"ip-10-0-101-15","ident":"kubelet","message":"E0323 15:05:36.670951    6922 remote_runtime.go:734] \\"ExecSync cmd from runtime service failed\\" err=\\"rpc error: code = DeadlineExceeded desc = failed to exec in container: timeout 5s exceeded: context deadline exceeded\\" containerID=\\"ac5e4fff7402033c291ba30ab91bff331c12bdea79c8dadedf73e5cf14af71a7\\" cmd=[/bitnami/scripts/readiness-probe.sh]","az":"us-east-1a","ec2_instance_id":"i-03672fb3a91ec2337"}']]
+    return read_s3('host')
 
 @pytest.fixture(scope='module')
 def init_host_expected_df():
@@ -187,4 +192,4 @@ def init_host_expected_df():
     # df = pd.DataFrame(data, columns=['log_timestamp', 'data'])
     # df['data'] = df.data.apply(json.loads)
     # return df
-    return read_df_s3('cp_kube_controller')
+    return read_df_s3('host')
