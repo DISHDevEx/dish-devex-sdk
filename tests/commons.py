5506884575af59036fd19c8d14c5aaf164e24ce8
Tomer Danon <tomer.danon@dish.com>
2023-04-17 15:20:30 +0000

    moved functions to commons, updated History.md

--- tests/commons.py
+++ tests/commons.py
@@ -19,3 +19,51 @@ def merge_master_schema(name, Schema, Spark, Spark_context):
     for item in obj.find_multilevel_schema_items(schema=merged_df.schema):
         merged_df = merged_df.withColumn(item, to_json(merged_df[item]))
     return merged_df
+
+# For GzConnector Testing
+gz_bucket_name = os.enviorn["GZ_BUCKET_NAME"]
+
+def read_s3(log_type, gz_bucket_name):
+    """
+    Read a text file from an S3 bucket for use in testing.
+
+    Parameters
+    ----------
+        log_type : str
+            The log type for which text file is being read.
+
+    Return
+    ------
+        contents : list
+            A list with one element, that is a list of the S3 object contents.
+            This is the required format for the test.
+    """
+    s3_resource = boto3.resource('s3')
+    key = f'pytest/txt_files/{log_type}_contents.txt'
+    obj = s3_resource.Object(bucket_name=gz_bucket_name, key=key)
+    body = obj.get()['Body'].read().decode('UTF-8')
+    contents = [[body]]
+    return contents
+
+def read_df_s3(log_type, gz_bucket_name):
+    """
+    Read a dataframe in pickle format from an S3 bucket for use in testing.
+
+    Parameters
+    ----------
+        log_type : str
+            The log type for which text file is being read.
+
+    Return
+    ------
+        df : dataframe
+            A Pandas dataframe.
+    """
+    s3_client = boto3.client('s3')
+    key = f'pytest/pickle_files/{log_type}_df.pickle'
+    response = s3_client.get_object(Bucket=gz_bucket_name, Key=key)
+    body = response['Body'].read()
+    df = pickle.loads(body)
+    return df
+
+
